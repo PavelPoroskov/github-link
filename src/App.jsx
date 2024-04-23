1eb4f7f633e7d43772e200607bb30e72a2da58ee
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-04-23 23:00:43 +0200

    fix dependencies: replace react-script with vite

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 
 import GitHubLink from './GitHubLink';
-/*
 import GitHubLinkList from './GitHubLinkList';
 
 const links = [
@@ -11,15 +10,15 @@ const links = [
   "https://github.com/facebook/relay",
   "https://github.com/reduxjs/redux",
   "https://github.com/reduxjs/react-redux",
-  "https://github.com/reduxjs/reselect",
-  "https://github.com/acdlite/recompose",
+  // "https://github.com/reduxjs/reselect",
+  // "https://github.com/acdlite/recompose",
   "https://github.com/mobxjs/mobx",
   "https://github.com/mobxjs/mobx-react",
   "https://github.com/ReactTraining/react-router",
   "https://github.com/reach/router",
   "https://github.com/cypress-io/cypress",
   "https://github.com/facebook/jest",
-  "https://github.com/webpack/webpack",
+  // "https://github.com/webpack/webpack",
   "https://github.com/babel/babel",
   "https://github.com/immutable-js/immutable-js",
   "https://github.com/immerjs/immer",
@@ -27,7 +26,6 @@ const links = [
   "https://github.com/nodejs/node",
   "https://github.com/facebook/create-react-app",
 ]
-*/
 
 function App() {
 
@@ -35,9 +33,9 @@ function App() {
     <div className="App">
       <GitHubLink link='https://github.com/facebook/create-react-app'/>
       <br/>
-      {/* 
+    
       <GitHubLinkList links={links}/>
-      */}
+
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
-it('renders without crashing', () => {
+it('renders without crashing', async () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  await act( async () => {
+    const root = ReactDOM.createRoot(div);
+    root.render(<App />)
+
+    root.unmount()
+  })
 });
